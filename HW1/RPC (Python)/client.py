f0e49d67266dd2c12ade52d7079170953f126b47
ManhLong <manhlongnshd2001@gmail.com>
2025-02-25 19:21:10 +0700

    Up RPC bai 1

---
+++ HW1/RPC (Python)/client.py
@@ -0,0 +1,14 @@
+import Pyro4
+
+# Dùng IP của máy server
+server_ip = "192.168.211.129"
+
+ns = Pyro4.locateNS(host=server_ip)  # Tìm tên server Pyro
+uri = ns.lookup("orders.service")  # Lấy uri service
+order_service = Pyro4.Proxy(uri)  # Connect to service
+
+# Gọi tới server
+product_id = "P0001"
+quantity = 3
+response = order_service.calculateTotal(product_id, quantity)
+print(response)
