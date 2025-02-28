7642f1c5417e971f3ef17e3c7fd8b113df6efbd6
ManhLong <manhlongnshd2001@gmail.com>
2025-02-28 22:39:53 +0700

    update

--- HW1/gRPC/client.py
+++ HW1/gRPC/client.py
@@ -1,38 +1,52 @@
+from locust import User, task, between, events
 import grpc
 import order_service_pb2
 import order_service_pb2_grpc
+import random
+import time
 
-from locust import HttpUser, task, between
-import random, time
+server_ip = "192.168.211.129:50051"  # IP server
 
-class GRPCUser(HttpUser):
-    wait_time = between(1, 3)  # Thời gian chờ giữa các request của mỗi user
+class GRPCClient:
+    def __init__(self):
+        self.channel = grpc.insecure_channel(server_ip)
+        self.stub = order_service_pb2_grpc.OrderServiceStub(self.channel)
+
+    def calculate_total(self, product_id, quantity):
+        request = order_service_pb2.OrderRequest(product_id=product_id, quantity=quantity)
+        return self.stub.CalculateTotal(request)
+
+class GRPCUser(User):
+    wait_time = between(1, 3)  # Thoi gian cho giua moi request
+    client = None
+
+    def on_start(self):
+        self.client = GRPCClient()
 
     @task
     def test_order_service(self):
-        channel = grpc.insecure_channel("192.168.211.129:50051")  # Change "localhost" to server IP if running on another machine
-        stub = order_service_pb2_grpc.OrderServiceStub(channel)
-
-        product_id = f"P{str(random.randint(1, 1000)).zfill(4)}"
+        # Random san pham, so luong
+        product_id = f"P{random.randint(1, 1000):04d}" 
         quantity = random.randint(1, 10)
 
         start_time = time.time()
         try:
-            request = order_service_pb2.OrderRequest(product_id=product_id, quantity=quantity)
+            response = self.client.calculate_total(product_id, quantity)
 
-            self.environment.events.request.fire(
+            total_time = int((time.time() - start_time) * 1000)
+            events.request.fire(
                 request_type="gRPC",
-                name="calculateTotal",
-                response_time=int((time.time() - start_time) * 1000),
-                response_length=len(str(response))
+                name="CalculateTotal",
+                response_time=total_time,
+                response_length=len(response.message)
             )
             
-            response = stub.CalculateTotal(request)
-            print(response.message)
+            print(response)
         except Exception as e:
-            self.environment.events.request.fire(
+            total_time = int((time.time() - start_time) * 1000)
+            events.request.fire(
                 request_type="gRPC",
-                name="calculateTotal",
-                response_time=int((time.time() - start_time) * 1000),
-                response_length=len(str(response))
+                name="CalculateTotal",
+                response_time=total_time,
+                exception=e
             )
