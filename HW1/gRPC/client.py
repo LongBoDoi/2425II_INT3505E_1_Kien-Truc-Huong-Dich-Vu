9b81b76f31fb41151dc3808f5cc6cb63e6c76f3b
ManhLong <manhlongnshd2001@gmail.com>
2025-02-28 18:18:44 +0700

    update

--- HW1/gRPC/client.py
+++ HW1/gRPC/client.py
@@ -3,7 +3,7 @@ import order_service_pb2
 import order_service_pb2_grpc
 
 from locust import HttpUser, task, between
-import random
+import random, time
 
 class GRPCUser(HttpUser):
     wait_time = between(1, 3)  # Thời gian chờ giữa các request của mỗi user
@@ -16,6 +16,23 @@ class GRPCUser(HttpUser):
         product_id = f"P{str(random.randint(1, 1000)).zfill(4)}"
         quantity = random.randint(1, 10)
 
-        request = order_service_pb2.OrderRequest(product_id=product_id, quantity=quantity)
-        response = stub.CalculateTotal(request)
-        print(response.message)
+        start_time = time.time()
+        try:
+            request = order_service_pb2.OrderRequest(product_id=product_id, quantity=quantity)
+
+            self.environment.events.request.fire(
+                request_type="gRPC",
+                name="calculateTotal",
+                response_time=int((time.time() - start_time) * 1000),
+                response_length=len(str(response))
+            )
+            
+            response = stub.CalculateTotal(request)
+            print(response.message)
+        except Exception as e:
+            self.environment.events.request.fire(
+                request_type="gRPC",
+                name="calculateTotal",
+                response_time=int((time.time() - start_time) * 1000),
+                response_length=len(str(response))
+            )
